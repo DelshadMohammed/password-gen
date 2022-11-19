966c6acad3c2e0b51b175336af251600e24d44be
Delshad Mohammed <116016974+DelshadMohammed@users.noreply.github.com>
2022-11-19 20:48:56 +0000

    added function and comments code is working 100%

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 // Assignment code here
-
+//message prompt for number
+var length = Number(
+    prompt("User Please Enter A Password Length Between 8 And 128")
+  ),
+  //prompt for special charchters
+  charType = prompt(
+    "User Please Enter A Character Type: special, numeric, uppercase, lowercase."
+  ),
+  passwordText = generatePassword();
+// function to generate random password
+function generatePassword() {
+  // my object literal for character sets to be used in gen
+  var charSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numeric: "0123456789",
+    special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  };
+  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+  var retVal = "";
+  //for loop used for random numbers and characters
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// object literalls used for reference
+var charSets = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +42,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
